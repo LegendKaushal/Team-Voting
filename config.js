a715302f075a4ad3d004385f7d2629bc94dc665c
LegendKaushal <ravichandra.bandi@gmail.com>
2021-09-15 19:42:55 +0530

    Version 1

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+    apiKey: "AIzaSyBqP9LXsHR2uI07_32sFLD215hUY0lz6co",
+    authDomain: "project-67-b2dde.firebaseapp.com",
+    databaseURL: "https://project-67-b2dde-default-rtdb.firebaseio.com",
+    projectId: "project-67-b2dde",
+    storageBucket: "project-67-b2dde.appspot.com",
+    messagingSenderId: "141353000210",
+    appId: "1:141353000210:web:1a401a5be797d3a78ba7cb",
+    measurementId: "G-CG3LHL7P3H"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
